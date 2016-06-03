bea8acde66e2af4ad43299945fbdabaa1cfcc2d5
Amanda Daly <techdaly@gmail.com>
2016-06-03 09:18:31 -0700

    Created AppComponent, displaying four example foods, details and calories

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,28 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Calorie Counter</h1>
+      <div *ngFor="#food of foods">
+        <h3>{{ food.name }}</h3>
+        <h4>{{ food.details }}</h4>
+        <h4>{{ food.calories }} calories</h4>
+      </div>
     </div>
   `
 })
 export class AppComponent {
+  public foods: Food[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.foods = [
+      new Food("Strawberry Cream Pie", "Yum, pie for breakfast!", 405, 0),
+      new Food("8 oz Coffee", "Cream, no sugar", 58, 1),
+      new Food("Grilled Cheese Sandwich", "Cheddar on wheat", 350, 2),
+      new Food("Tomato Soup", "Cup, very delicious, little cheese on top", 280, 3)
+    ];
+  }
+}
+export class Food {
+  constructor(public name: string, public details: string, public calories: number, public id: number){
 
+  }
 }
